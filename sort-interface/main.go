258429be1952d03b98fb0165d87f301df9f28084
Sachin Thatte <sthatte@sthatte-m01.vmware.com>
2019-07-02 18:35:19 -0500

    added number 6 to comment

---
+++ sort-interface/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+type SortAny interface {
+	SortSlice()
+}
+
+//my comment 1 2 3 4 5
+type people []string
+type nums []int
+
+var studygroup = people{"Zeno", "John", "Al", "Jenny"}
+var mynums = nums{7, 4, 8, 2, 9, 19, 12, 32, 3}
+
+func (p people) SortSlice() {
+	sort.Sort(sort.Reverse(sort.StringSlice(studygroup)))
+	//sort.Strings(studygroup)
+	fmt.Println(studygroup)
+}
+func (n nums) SortSlice() {
+	sort.Ints(mynums)
+	fmt.Println(mynums)
+}
+
+func SortThis(s SortAny) {
+	s.SortSlice()
+}
+func main() {
+	SortThis(studygroup)
+	studygroup.SortSlice()
+	SortThis(mynums)
+	mynums.SortSlice()
+
+}
